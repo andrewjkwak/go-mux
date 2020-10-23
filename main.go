6ab3d0ae8ff15e403fb78715f2b5d072b51c42c7
Andrew Kwak <andrewjkwak@gmail.com>
2020-10-23 13:04:02 -0400

    Built product API using golang and gorilla mux

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+}
+
+func main() {
+	a := App{}
+	a.Initialize(
+		os.Getenv("APP_DB_USERNAME"),
+		os.Getenv("APP_DB_PASSWORD"),
+		os.Getenv("APP_DB_NAME"),
+	)
+
+	a.Run(":8010")
+}
